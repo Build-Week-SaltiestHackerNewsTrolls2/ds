a275282bf2aa9ce1f3b71d271b902279098ec083
Luis Robles <lfr4704@gmail.com>
2020-06-18 19:59:45 -0700

    created json output

--- saltyapp/__init__.py
+++ saltyapp/__init__.py
@@ -11,3 +11,5 @@ def create_app():
 if __name__ =="__main__":
     myapp = create_app()
     myapp.run(debug=True)
+
+    
--- saltyapp/routes/user_routes.py
+++ saltyapp/routes/user_routes.py
@@ -1,9 +1,26 @@
 #saltyapp/routes
 
 from flask import Blueprint, jsonify
+import pandas as pd
 
 user_routes = Blueprint("user_routes", __name__)
 
 @user_routes.route('/')
 def user():
     return "hello world"
+
+@user_routes.route('/saltyscore/<username>')
+def salty_score(username):
+    print(username)
+    my_df = pd.DataFrame([4.5, 5, -5],
+    index=['luis', 'ilmo', 'becca'],
+    columns=['saltyscore']
+    )
+
+    result = my_df.loc[username]
+    print(result)
+    print(type(result.to_json()))
+    return result.to_json()
+
+def username_list():
+    pass
