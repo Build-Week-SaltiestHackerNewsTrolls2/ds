94d56788049ea75f4bdc93fc91edaa8f7a633106
johnnykoo84 <johnnykoo84@gmail.com>
2020-06-23 17:11:22 +0900

    Try to connect DB with ORM

--- saltyapp/__init__.py
+++ saltyapp/__init__.py
@@ -1,33 +1,31 @@
-#saltyapp/__init__.py
+# saltyapp/__init__.py
 
+from saltyapp.routes import user_routes
+from saltyapp.model import db
 from flask import Flask
-from saltyapp.routes.user_routes import user_routes
 import os
 from dotenv import load_dotenv
+load_dotenv()
 
 # how to hook db into flask reference
 # https://vsupalov.com/flask-sqlalchemy-postgres/
-
-load_dotenv()
-
 DB_USERNAME = os.getenv('DB_USERNAME')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_NAME = os.getenv('DB_NAME')
 
-DB_URL = 'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+DB_URL = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(user_routes)
-
+    app.register_blueprint(user_routes.user_routes)
+    db.init_app(app)
     app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
     app.config['SQALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    db = SQLAlchemy(app)
-
     return app
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     myapp = create_app()
     myapp.run(debug=True)
--- saltyapp/routes/user_routes.py
+++ saltyapp/routes/user_routes.py
@@ -1,19 +1,21 @@
-#saltyapp/routes
+# saltyapp/routes
 
 from flask import Blueprint, jsonify
 import pandas as pd
-from saltyapp.model import db, User, parse_records
+from saltyapp.model import db, salty_user, salty_comment, parse_records
 
 user_routes = Blueprint("user_routes", __name__)
-
 my_df = pd.DataFrame([4.5, 5, -5, -50, 45, 56, 99, 100, -1, -76, 83, 55],
-index=['luis', 'ilmo', 'becca', 'pyrom', 'daniel', 'kara', 'adam', 'zack', 'ethan', 'mistery', 'manny', 'rosa'],
-columns=['saltyscore']
-)
+                     index=['luis', 'ilmo', 'becca', 'pyrom', 'daniel', 'kara',
+                            'adam', 'zack', 'ethan', 'mistery', 'manny', 'rosa'],
+                     columns=['saltyscore']
+                     )
+
 
 @user_routes.route('/')
 def user():
     return "hello world"
+
 
 @user_routes.route('/saltyscore/<username>')
 def salty_score(username):
@@ -22,32 +24,35 @@ def salty_score(username):
     #result = my_df.loc[username]
     print(result)
 
-    result = User.query.filter(User.Username = username)
+    result = salty_user.query.filter_by(Username=username)
     print(type(result.to_json()))
 
-
     return result.to_json()
+
 
 @user_routes.route('/topfive')
 def top_five():
     #topfive = my_df.sort_values(by=['saltyscore'])
-    topfive = User.query.limit(5).all()
+    topfive = salty_user.query.limit(5).all()
     #data = topfive[:5].to_json()
     data = jsonify(topfive)
     return data
 
+
 @user_routes.route('/users')
 def username_list():
-    print(my_df.to_json(orient='index'))
-    json_df = my_df.to_json(orient='index')
-    records = User.query(User.User_id, User.Username).all()
+    # print(my_df.to_json(orient='index'))
+    # json_df = my_df.to_json(orient='index')
+    records = salty_user.query(User_ID, Username).all()
+    print('@@@ records', records)
     json_list = parse_records(records)
 
     return json_list
 
+
 @user_routes.route('/comment/<username>')
 def comment_list(username):
-    records = Comment.query.filter(Comment.Username = username)
+    records = salty_comment.query.filter_by(Username=username)
     comments = parse_records(records)
     json_comments = jsonify(comments)
     return comments
